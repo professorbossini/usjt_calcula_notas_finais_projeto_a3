73f15da3bee827974d7732240dbfa1dad7a6ad71
Rodrigo Bossini <professorbossini@gmail.com>
2021-12-12 16:54:32 -0300

    Nova versao. Problema resolvido

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 import statistics
 import csv
+import re
+
 i = {
     'qtdeMembros': 20,
     'inicio1Membros': 21,
@@ -18,15 +20,19 @@ i = {
     'finalNotas': 14,
     'destaque': 15,
     'nomeDoGrupoPlanilhaNotas': 5,
+    'campus': 2,
+    'turno': 5
 
 }
 
 def buildCompleteTeam (linha, qtdeMembros, inicio):
-    team = {'alunos': []}
+    team = defaultdict(list)
     for j in range(inicio, inicio + 2* qtdeMembros - 1, 2):
-        team['alunos'].append({'ra': linha[j+1], 'nome': linha[j].replace('\t', '').strip()})
+        team['alunos'].append({'ra': re.findall('[0-9]+',linha[j+1])[0], 'nome': linha[j].replace('\t', '').strip()})
     team['nomeDoGrupo'] = linha[i['nomeDoGrupo']].replace('\t', '').strip()
     team['tipoProjeto'] = linha[i['tipoProjeto']].replace('\t', '').strip()
+    team['campus'] = linha[i['campus']].replace('\t', '').strip()
+    team['turno'] = linha[i['turno']].replace('\t', '').strip()
     return team
 
 def buildAllTeams ():
@@ -40,61 +46,49 @@ def buildAllTeams ():
         teams = sorted(teams, key=lambda x: x['nomeDoGrupo'].replace('\t', '').strip())
     return teams
 
-def calculateAllRasWithGrades ():
-    rasWithGrades = []
+def calculateAllRasWithGrades (allTeams):
     with open (r'notas.csv', encoding='utf-8') as file_notas:
         leitor2 = csv.reader(file_notas, delimiter=',')
         for linha2 in leitor2:
             ra = linha2[i['raUnicoComNota']].replace('\t', '').strip()
-            for raWithGrade in rasWithGrades:
-                if raWithGrade['ra'] == ra:
-                    raWithGrade['notas'].append(statistics.mean(map(int,linha2[i['inicioNotas']:i['finalNotas']])))
-                    if 'N' in raWithGrade['destaque']:
-                        raWithGrade['destaque'] = linha2[i['destaque']]
-                    break
-            else:
-                rasWithGrades.append({
-                    'ra': ra, 
-                    'notas':[statistics.mean(map(int,linha2[i['inicioNotas']:i['finalNotas']]))],
-                    'destaque': linha2[i['destaque']],
-                    'nomeDoGrupo':linha2[i['nomeDoGrupoPlanilhaNotas']].replace('\t', '').strip()
-                })
-    return rasWithGrades
-rasWithGrades = calculateAllRasWithGrades()
-for r in rasWithGrades:
-    print (r['nomeDoGrupo'])
+            teamItBelongsTo = None
+            for team in allTeams:
+                for aluno in team['alunos']:
+                    if aluno['ra'] == ra:
+                        teamItBelongsTo = team
+                        teamItBelongsTo['notas'].append(
+                            statistics.mean([int(a) for a in linha2[i['inicioNotas']:i['finalNotas']]])
+                        )
+                        if 'N' in teamItBelongsTo['destaque'] or teamItBelongsTo['destaque'] == []:
+                            teamItBelongsTo['destaque'] = linha2[i['destaque']]
+                        break
+    #calcular medias finais
+    for team in allTeams:
+        team['mediaFinal'] = 0 if len(team['notas']) <= 0 else statistics.mean(team['notas'])
 allTeams = sorted(buildAllTeams(), key=lambda x: x['nomeDoGrupo'])
 
+calculateAllRasWithGrades(allTeams)
+# for t in allTeams:
+#     print (t['nomeDoGrupo'], t['notas'], t['mediaFinal'])
 
-finalGradesPerGroup = []
-for team in allTeams:
-    for raWithGrade in rasWithGrades:
-        #muitos grupos diferentes com nome igual
-        #grupos com nome errado (exemplo: Outro)
-        #não vai dar para usar na junção
-        # if any (map (lambda x: x['ra'] == raWithGrade['ra'], team['alunos'])) and team['nomeDoGrupo'] == raWithGrade['nomeDoGrupo']:
-        if any (map (lambda x: x['ra'] == raWithGrade['ra'], team['alunos'])):
-            finalGradesPerGroup.append({
-                'alunos': team['alunos'],
-                'nomeDoGrupo': team['nomeDoGrupo'],
-                'tipoProjeto': team['tipoProjeto'],
-                'mediaFinal': statistics.mean(map(int, raWithGrade['notas'])),
-                'destaque': raWithGrade['destaque']
-            })
+
+arquivoFinal = open ('20212_usjt_expo_notas_finais.csv', 'w', encoding='UTF-8')
+arquivoFinal.writelines ('ra,nome,mediaFinal,campus,turnonomeGrupo,tipoProjeto,Destaque?\n')
+for t in allTeams:
+    for a in t['alunos']:
+        resultingString = f'{a["ra"]},{a["nome"]},{t["mediaFinal"] * 2},{t["campus"]},{t["turno"]},{t["nomeDoGrupo"].replace(",","")},{t["tipoProjeto"].replace(",","")},{0 if t["destaque"] == [] else t["destaque"]}'
+        print(resultingString)
+        arquivoFinal.writelines(resultingString)
+        arquivoFinal.writelines('\n')
+    
+
+
+
 
 # for f in finalGradesPerGroup:
 #     print (f['nomeDoGrupo'])
 
-arquivoFinal = open ('20212_usjt_expo_notas_finais.csv', 'w', encoding='UTF-8')
-arquivoFinal.writelines ('ra,nome,mediaFinal,nomeGrupo,tipoProjeto,Destaque?\n')
-for f in finalGradesPerGroup:
-    for aluno in f['alunos']:
-        resultingString = f'{aluno["ra"]},{aluno["nome"]},{f["mediaFinal"] / 5 * 10:.2f},{f["nomeDoGrupo"]},{f["tipoProjeto"]},{"Sim" if f["mediaFinal"] / 5 * 10 >= 10 and "S" in f["destaque"] else "Não"}'
-        print (
-            resultingString
-        )
-        arquivoFinal.writelines(resultingString)
-        arquivoFinal.writelines('\n')
+
 
 
 
